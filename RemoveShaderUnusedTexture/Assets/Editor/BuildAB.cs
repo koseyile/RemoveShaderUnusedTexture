a7acb7a092b0230fbdf87e85d23b8b061a0dc191
chenyuan <chenyuanzhang@hotmail.com>
2018-07-25 16:14:58 +0800

    make sure if there is a unused texture in a shader. build assetbundle will contain the texture.

---
+++ RemoveShaderUnusedTexture/Assets/Editor/BuildAB.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BuildAB : MonoBehaviour {
+
+	[MenuItem("Build Asset Bundles/Normal")]
+	static void BuildABsNone()
+	{
+		//Create a folder to put the Asset Bundle in.
+		// This puts the bundles in your custom folder (this case it's "MyAssetBuilds") within the Assets folder.
+		//Build AssetBundles with no special options
+		BuildPipeline.BuildAssetBundles("Assets/StreamingAssets", BuildAssetBundleOptions.None, BuildTarget.StandaloneOSXIntel64);
+	}
+}
