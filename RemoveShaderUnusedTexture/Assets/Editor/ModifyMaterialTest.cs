2f74743f77596abe0f4181cd868cd3d7e86b445e
chenyuan <chenyuanzhang@hotmail.com>
2018-07-26 17:23:42 +0800

    analyze shader file and remove unused texture

---
+++ RemoveShaderUnusedTexture/Assets/Editor/ModifyMaterialTest.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class ModifyMaterialTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	[MenuItem ("ChangeMaterial/SetTextureToNull")]
+	static void SetTextureToNull(){
+		//string path = Application.dataPath + "/Unlit_testShader.mat";
+		string path = "Assets/Unlit_testShader.mat";
+		Debug.Log (path);
+		Material m = AssetDatabase.LoadAssetAtPath<Material> (path);
+		if (m == null) {
+			//Debug.Log ("nnnn");
+		}
+
+		string shaderPath = AssetDatabase.GetAssetPath (m.shader.GetInstanceID ());
+		Debug.Log (shaderPath);
+
+		List<string> textureNameList = new List<string>();
+		getUnusedTextureName (shaderPath, textureNameList);
+
+		foreach (var i in textureNameList) {
+			//Debug.Log (i);
+			m.SetTexture (i, null);
+		}
+
+		//m.mainTexture = null;
+		//m.SetTexture ("_MainTex", null);
+		AssetDatabase.SaveAssets ();
+		//AssetDatabase.Refresh ();
+	}
+
+	static void getUnusedTextureName(string shaderPath, List<string> textureNameList)
+	{
+		StreamReader sr = new StreamReader (shaderPath);
+
+		while (!sr.EndOfStream) {
+			string line = sr.ReadLine ();
+			//Debug.Log (line);
+
+			line = line.Trim ();
+
+			if (line.StartsWith ("//")) {
+				//Debug.Log (line);
+				continue;
+			}
+
+			if (line.StartsWith ("sampler2D")) {
+				//Debug.Log (line);
+				int index = line.IndexOf ("sampler2D")+"sampler2D".Length;
+				string texVariable = line.Substring (index);
+				texVariable = texVariable.Trim ();
+				char[] trimChar = {';'};
+				texVariable = texVariable.TrimEnd (trimChar);
+				textureNameList.Add (texVariable);
+				//Debug.Log (texVariable);
+				continue;
+			}
+
+			List<string> removeStringList = new List<string> ();
+			foreach(var tn in textureNameList){
+				if (line.Contains (tn)) {
+					//Debug.Log ("check "+ line);
+					removeStringList.Add(tn);
+				}
+			}
+
+			foreach (var rs in removeStringList) {
+				//Debug.Log ("Remove " + rs);
+				textureNameList.Remove(rs);
+			}
+		
+
+		}
+
+
+		//textureNameList.Add ("_MainTex");
+	}
+}
--- RemoveShaderUnusedTexture/Assets/testShader.shader
+++ RemoveShaderUnusedTexture/Assets/testShader.shader
@@ -2,7 +2,9 @@
 {
 	Properties
 	{
-		_MainTex ("Texture", 2D) = "white" {}
+		_MainTex1 ("Texture", 2D) = "white" {}
+		_MainTex2 ("Texture", 2D) = "white" {}
+		_MainTex3 ("Texture", 2D) = "white" {}
 	}
 	SubShader
 	{
@@ -32,14 +34,16 @@
 				float4 vertex : SV_POSITION;
 			};
 
-			sampler2D _MainTex;
-			float4 _MainTex_ST;
+			sampler2D _MainTex1;
+			sampler2D _MainTex2;
+			sampler2D _MainTex3;
+			float4 _MainTex1_ST;
 			
 			v2f vert (appdata v)
 			{
 				v2f o;
 				o.vertex = UnityObjectToClipPos(v.vertex);
-				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex1);
 				UNITY_TRANSFER_FOG(o,o.vertex);
 				return o;
 			}
@@ -47,8 +51,10 @@
 			fixed4 frag (v2f i) : SV_Target
 			{
 				// sample the texture
-				fixed4 col = 1;//tex2D(_MainTex, i.uv);
-				//fixed4 col = tex2D(_MainTex, i.uv);
+				//fixed4 col = 1;
+				//tex2D(_MainTex, i.uv);
+				fixed4 col = tex2D(_MainTex1, i.uv);
+				col = tex2D(_MainTex2, i.uv);
 				// apply fog
 				UNITY_APPLY_FOG(i.fogCoord, col);
 				return col;
